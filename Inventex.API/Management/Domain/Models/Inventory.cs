4f779479a19aab533d46e32b576de59f5135329e
Samuel Maucaille <antonio0230@hotmail.com>
2022-06-07 21:45:52 -0500

    feat: added Inventory and more
    Inventory.cs
    InventoryResponse.cs
    InventoryService.cs

---
+++ Inventex.API/Management/Domain/Models/Inventory.cs
@@ -0,0 +1,15 @@
+﻿namespace Inventex.API.Management.Domain.Models;
+
+
+public class Inventory
+    {
+        public string Name { get; set; }
+        public byte[] Image { get; set; }
+        public float Price { get; set; }
+        public string Category { get; set; }
+        public string InvetoryStatus { get; set; }
+
+        //Relationships 
+        
+        public User User { get; set; }
+    }
