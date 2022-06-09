bb006ee4acb2526f42e9c1f374059b7e5e10b32f
Samuel Maucaille <antonio0230@hotmail.com>
2022-06-08 21:22:36 -0500

    feat: Inventory Service
    PUT POST Implementation

--- Inventex.API/Management/Persistence/Repositories/InventoryRepository.cs
+++ Inventex.API/Management/Persistence/Repositories/InventoryRepository.cs
@@ -1,6 +1,38 @@
-﻿namespace Inventex.API.Management.Persistence.Repositories;
+﻿using Inventex.API.Management.Domain.Models;
+using Inventex.API.Management.Domain.Repositories;
+using Inventex.API.Management.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
 
-public class InventoryRepository
+namespace Inventex.API.Management.Persistence.Repositories;
+
+public class InventoryRepository : BaseRepository, IInventoryRepository
 {
-    
+    public InventoryRepository(AppDbContext context) : base(context)
+    {
+    }
+
+    public async Task<IEnumerable<Inventory>> ListAsync()
+    {
+        return await _context.Inventories.ToListAsync();
+    }
+
+    public async Task AddAsync(Inventory inventory)
+    {
+        await _context.Inventories.AddAsync(inventory);
+    }
+
+    public async Task<Inventory> FindByIdAsync(int id)
+    {
+        return await _context.Inventories.FindAsync(id);
+    }
+
+    public void Update(Inventory inventory)
+    {
+        _context.Inventories.Update(inventory);
+    }
+
+    public void Remove(Inventory inventory)
+    {
+        _context.Inventories.Remove(inventory);
+    }
 }
---
+++ Inventex.API/Management/Services/InventoryService.cs
@@ -0,0 +1,65 @@
+﻿using Inventex.API.Management.Domain.Models;
+using Inventex.API.Management.Domain.Repositories;
+using Inventex.API.Management.Domain.Services;
+using Inventex.API.Management.Domain.Services.Communication;
+
+namespace Inventex.API.Management.Services;
+
+public class InventoryService : IInventoryService
+{
+    private readonly IInventoryRepository _inventoryRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public InventoryService(IInventoryRepository inventoryRepository, IUnitOfWork unitOfWork)
+    {
+        _inventoryRepository = inventoryRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async  Task<IEnumerable<Inventory>> ListAsync()
+    {
+        return await _inventoryRepository.ListAsync();
+    }
+    
+    //POST
+    public async Task<InventoryResponse> SaveAsync(Inventory inventory)
+    {
+        try
+        {
+            await _inventoryRepository.AddAsync(inventory);
+            await _unitOfWork.CompleteAsync();
+            return new InventoryResponse(inventory);
+        }
+        catch (Exception e)
+        {
+            return new InventoryResponse($"An error occurred while saving inventory item: {e.Message}");
+        }
+    }
+
+    //PUT
+    public async Task<InventoryResponse> UpdateAsync(int id, Inventory inventory)
+    {
+        var existingInventory = await _inventoryRepository.FindByIdAsync(id);
+        if (existingInventory == null)
+            return new InventoryResponse("Inventory item not found");
+
+        existingInventory.Name = inventory.Name;
+        try
+        {
+            _inventoryRepository.Update(existingInventory);
+            await _unitOfWork.CompleteAsync();
+
+            return new InventoryResponse(existingInventory);
+        }
+        catch (Exception e)
+        {
+            return new InventoryResponse($"An error occurred while updating inventory item: {e.Message}");
+        }
+    }
+    
+    //DELETE
+    public Task<InventoryResponse> DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
