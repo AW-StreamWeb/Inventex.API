5f5edf915e0fb6180661602f7a198b0e69e153cc
Samuel Maucaille <antonio0230@hotmail.com>
2022-06-11 08:06:02 -0500

    feat(tutorials):
    Added incoming and outgoing resources

--- Inventex.API/Management/Resources/SaveInventoryResource.cs
+++ Inventex.API/Management/Resources/SaveInventoryResource.cs
@@ -1,12 +1,24 @@
-﻿namespace Inventex.API.Management.Resources;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Inventex.API.Management.Resources;
 
 public class SaveInventoryResource
 {
+    [Required]
+    [MaxLength(50)]
     public string Name { get; set; }
+    
     public byte[] Image { get; set; }
+    
+    [MaxLength(10)]
     public float Price { get; set; }
+    
+    [MaxLength(50)]
     public string Category { get; set; }
+
+    [MaxLength(50)]
     public string InventoryStatus { get; set; }
     
-    public UserResource User { get; set; }
+    [Required]
+    public int UserId { get; set; }
 }
